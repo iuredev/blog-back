c0ffd755510b9cde54b01e5a8fb1c49f7d6e752a
Iure Silva <iure.sgz@gmail.com>
2025-02-11 11:47:11 -0300

    add: routes from articles, and static pages

---
+++ src/api/manual/controllers/manual.ts
@@ -0,0 +1,7 @@
+/**
+ * manual controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::manual.manual');
---
+++ src/api/manual/routes/manual.ts
@@ -0,0 +1,7 @@
+/**
+ * manual router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::manual.manual');
